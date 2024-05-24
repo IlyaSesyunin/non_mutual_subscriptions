808e96b8cfa395db3f6250bb4accecc0647c9b2f
Ilya Sesyunin <ilia.sesiunin@samgau.com>
2024-05-24 15:38:51 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # non_mutual_subscriptions
+
 The program helps you find unsubscribed subscribers you follow
+
+## Instruction
+
+1. Download an archive with information about yourself from
+   Instagram: https://accountscenter.instagram.com/info_and_permissions/dyi/
+2. Add the files _following.json_ and _followers_1.json_ from the archive to the data folder.
+3. Press Start.
---
+++ find_unfollowers.py
@@ -0,0 +1,29 @@
+import json
+
+# Load data from the JSON files
+with open('data/following.json', 'r') as f:
+    following_data = json.load(f)['relationships_following']
+
+with open('data/followers_1.json', 'r') as f:
+    followers_data = json.load(f)
+
+
+# Function to extract hrefs from the provided data structure
+def extract_hrefs(data):
+    hrefs = set()
+    for item in data:
+        for string_list_item in item['string_list_data']:
+            hrefs.add(string_list_item['href'])
+    return hrefs
+
+
+# Extract the href values
+following_links = extract_hrefs(following_data)
+followers_links = extract_hrefs(followers_data)
+
+# Find links that are in following but not in followers
+unique_following_links = following_links - followers_links
+
+# Print the results
+for link in unique_following_links:
+    print(link)
